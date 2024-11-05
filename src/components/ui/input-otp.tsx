b38764954f9c3b6a0032b3a375fdfb104e5cc6a8
KanonKC <kanonkce@gmail.com>
2024-11-05 10:50:26 +0700

    Responsive on JoinLobby page

--- src/components/ui/input-otp.tsx
+++ src/components/ui/input-otp.tsx
@@ -39,7 +39,7 @@ const InputOTPSlot = React.forwardRef<
     <div
       ref={ref}
       className={cn(
-        "relative flex h-20 w-20 items-center justify-center border-y border-r border-input text-4xl transition-all first:rounded-l-md first:border-l last:rounded-r-md",
+        "relative flex h-10 w-10 text-xl md:h-20 md:w-20 items-center justify-center border-y border-r border-input md:text-4xl transition-all first:rounded-l-md first:border-l last:rounded-r-md",
         isActive && "z-10 ring-2 ring-ring ring-offset-background",
         className
       )}
--- src/views/JoinLobby.tsx
+++ src/views/JoinLobby.tsx
@@ -1,56 +1,62 @@
-import { Button } from '@/components/ui/button'
-import { InputOTP, InputOTPGroup, InputOTPSlot } from '@/components/ui/input-otp'
-import CenterContainer from '@/layouts/CenterContainer'
-import { PlaylistService } from '@/services/apis/Playlist.service'
-import { useCallback, useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Button } from "@/components/ui/button";
+import {
+	InputOTP,
+	InputOTPGroup,
+	InputOTPSlot,
+} from "@/components/ui/input-otp";
+import CenterContainer from "@/layouts/CenterContainer";
+import { PlaylistService } from "@/services/apis/Playlist.service";
+import { useCallback, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const JoinLobby = () => {
+	const [roomCode, setRoomCode] = useState("");
+	const navigate = useNavigate();
 
-    const [roomCode,setRoomCode] = useState("")
-    const navigate = useNavigate()
+	const handleChange = (e: string) => {
+		setRoomCode(e);
+	};
 
-    const handleChange = (e:string) => {
-        setRoomCode(e)
-    }
+	const resolveRoomCode = useCallback(async () => {
+		PlaylistService.get(roomCode)
+			.then(() => {
+				navigate(`/${roomCode}`);
+			})
+			.catch(() => {});
+	}, [roomCode, navigate]);
 
-    const resolveRoomCode = useCallback(async () => {
-        PlaylistService.get(roomCode).then(() => {
-            navigate(`/${roomCode}`)
-        }).catch(() => {
-            
-        })
-    },[roomCode,navigate])
+	return (
+		<CenterContainer>
+			<div className="">
+				<div>
+					<InputOTP
+						onComplete={resolveRoomCode}
+						onChange={(e) => handleChange(e)}
+						value={roomCode}
+						maxLength={6}
+					>
+						<InputOTPGroup>
+							<InputOTPSlot index={0} />
+							<InputOTPSlot index={1} />
+							<InputOTPSlot index={2} />
+							<InputOTPSlot index={3} />
+							<InputOTPSlot index={4} />
+							<InputOTPSlot index={5} />
+						</InputOTPGroup>
+					</InputOTP>
+				</div>
 
-    useEffect(() => {
-        if (roomCode.length === 6) {
-            resolveRoomCode()
-        }
-    },[roomCode,resolveRoomCode])
+				<div className="flex justify-center mt-5">
+					<Button
+						disabled={roomCode.length !== 6}
+						onClick={resolveRoomCode}
+					>
+						Join Lobby
+					</Button>
+				</div>
+			</div>
+		</CenterContainer>
+	);
+};
 
-  return (
-    <CenterContainer>
-
-        <div className=''>
-           <div>
-                <InputOTP onChange={(e) => handleChange(e)} value={roomCode} maxLength={6}>
-                    <InputOTPGroup>
-                        <InputOTPSlot index={0} />
-                        <InputOTPSlot index={1} />
-                        <InputOTPSlot index={2} />
-                        <InputOTPSlot index={3} />
-                        <InputOTPSlot index={4} />
-                        <InputOTPSlot index={5} />
-                    </InputOTPGroup>
-                </InputOTP>
-           </div>
-
-           <div className='flex justify-center mt-5'>
-                <Button disabled={roomCode.length !== 6} onClick={resolveRoomCode}>Join Lobby</Button>
-           </div>
-        </div>
-    </CenterContainer>
-  )
-}
-
-export default JoinLobby
+export default JoinLobby;
