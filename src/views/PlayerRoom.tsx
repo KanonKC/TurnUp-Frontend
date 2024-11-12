2fda032c1164d1c56b999b42588235a88660501d
KanonKC <kanonkce@gmail.com>
2024-11-12 16:34:22 +0700

    Remove console.log

--- src/views/PlayerRoom.tsx
+++ src/views/PlayerRoom.tsx
@@ -24,7 +24,6 @@ const PlayerRoom = () => {
 
 		QueueService.getAll(playlistId)
 			.then((response) => {
-                console.log("Loaded data", response.data.data);
 				setQueues(response.data.data);
 				return PlaylistService.get(playlistId);
 			})
