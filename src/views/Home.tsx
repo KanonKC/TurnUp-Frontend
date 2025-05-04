b8ef594b252d32e89ce68502b164c647d4324591
KanonKC <kanonkce@gmail.com>
2025-05-05 02:11:00 +0700

    Add creator contract in home

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,3 +1,4 @@
+import CreatorContact from "@/components/CreatorContact";
 import RecentlyVisitedPlaylistList from "@/components/RecentlyVisitedPlaylistList";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
@@ -9,9 +10,9 @@ const Home = () => {
 
 	return (
 		<CenterContainer hideBackButton>
-            <div className="themed-color text-center text-4xl sm:text-5xl font-bold">
-                TURN UP
-            </div>
+			<div className="themed-color text-center text-4xl sm:text-5xl font-bold">
+				TURN UP
+			</div>
 			<div className="flex flex-col gap-2 mt-10">
 				<Button className="px-24" onClick={() => navigate("/create")}>
 					Create a Lobby
@@ -28,7 +29,10 @@ const Home = () => {
 				>
 					Join an existing Lobby
 				</Button>
-                <RecentlyVisitedPlaylistList />
+				<RecentlyVisitedPlaylistList />
+				<div className="mt-5">
+					<CreatorContact/>
+				</div>
 			</div>
 		</CenterContainer>
 	);
